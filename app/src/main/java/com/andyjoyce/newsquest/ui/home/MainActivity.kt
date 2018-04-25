3483b9bd3ba5da5997849ca4138d73570c3980ce
Andy Joyce <>
2018-04-25 14:51:16 +0100

    Updated Packages
    Implemented multiple packages

--- app/src/main/java/com/andyjoyce/newsquest/ui/home/MainActivity.kt
+++ app/src/main/java/com/andyjoyce/newsquest/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.andyjoyce.newsquest
+package com.andyjoyce.newsquest.ui.home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.andyjoyce.newsquest.R
 
 class MainActivity : AppCompatActivity() {
 
